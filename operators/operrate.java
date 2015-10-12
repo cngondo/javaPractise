1e4d0da88c66cff280f3d86b1626940b8b8af7a7
cngondo <devngondo@gmail.com>
2015-10-12 15:24:04 +0300

    Operators in java

---
+++ operators/operrate.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class apples{
+  public static void main(String[] args){
+    Scanner corn = new Scanner(System.in);
+    // var declarations
+    int tuna = 5;
+    int bass = 18;
+    // Increments the var before usage
+    // ++tuna;
+    // Increments after usage
+    // tuna++
+    // Increments with 8 and assigns to new var
+    bass+=8;
+    System.out.println(tuna++);
+    System.out.println(tuna);
+    System.out.println(bass);
+
+  }
+}
